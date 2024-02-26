494863bfafcc9486ac61a71c010350ede8099618
paul <paul002706@gmail.com>
2024-02-26 20:21:29 +0800

    brrrrrrrrrrrrrrt

---
+++ src/main/java/com/chiliasmstudio/ResidenceManager.java
@@ -0,0 +1,17 @@
+package com.chiliasmstudio;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ResidenceManager extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
